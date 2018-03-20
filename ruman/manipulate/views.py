4673c89a281abf60e747895a7e6c1e2744a5781b
NoahChan <NoahChan666@gmail.com>
2018-03-20 14:33:38 +0800

    cyz

--- ruman/manipulate/views.py
+++ ruman/manipulate/views.py
@@ -2,12 +2,12 @@
 #encoding: utf-8
 
 from flask import Flask, render_template, request, jsonify, Blueprint, send_from_directory, url_for, session
-from economy.db import *
+from ruman.db import *
 from . import homePage
 import json
-from economy.config import *
-from economy.entityPortrait import views
-from economy.es import *
+from ruman.config import *
+
+from ruman.es import *
 
 @manipulate.route('/')
 def index():
