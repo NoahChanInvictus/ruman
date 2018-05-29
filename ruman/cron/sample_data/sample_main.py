b45ed8ce74a2839b893bb902a9382746f8b3fb0f
linhaobuaa <1257819385@qq.com>
2018-05-29 21:06:14 +0800

    cyz

--- ruman/cron/sample_data/sample_main.py
+++ ruman/cron/sample_data/sample_main.py
@@ -16,7 +16,7 @@ def get_traceback(sampleid,data):
     selected = sample_data.loc[sample_data['id']==sampleid].fillna('nan')
     selected_dict = selected.to_dict(orient= 'records')
     for item in selected_dict:
-        item['publish_time'] = str(weibo_data['publish_time'])
+        item['publish_time'] = str(item['publish_time'])
     selected_dict = sorted(selected_dict,key = lambda x:x['publish_time'])
     return selected_dict
 
@@ -36,6 +36,7 @@ def weibo_fix(weibo_data):
                 weibo_data['id'][index] = int(real_mid)
                 # print type(weibo_data['publish_time'])
                 weibo_data['publish_time'][index] = str(weibo_data['publish_time'][index])
+                
                 break
     # print weibo_data['id']
     return weibo_data
